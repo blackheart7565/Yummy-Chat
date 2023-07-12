779eae99f50baa78e46aac6c467f4773cc2f5477
blackheart7565 <give.me.heart8844@gmail.com>
2023-07-12 14:45:27 +0300

    Create WebSocket

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const {WebSocket, WebSocketServer} = require("ws");
+
+const PORT = 7000;
+
+const wss = new WebSocketServer({
+    port: PORT
+});
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(message);
+        message = JSON.parse(message);
+
+        switch (message.event) {
+            case 'message': {
+                broadcastMessage(message);
+            }break;
+            case 'connecting': {
+                broadcastMessage(message);
+            } break;
+        }
+    });
+});
+
+function broadcastMessage(message) {
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(message));
+    })
+}
