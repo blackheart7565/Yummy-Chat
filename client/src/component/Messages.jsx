1d3f19f79a480c51287fcf9a8175261d7dc3bf5a
blackheart7565 <121513652+blackheart7565@users.noreply.github.com>
2023-08-03 17:09:21 +0300

    Added auto scroll messages to the bottom

--- client/src/component/Messages.jsx
+++ client/src/component/Messages.jsx
@@ -1,18 +1,39 @@
 import mgs from '../styles/module/Messages.module.scss';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import Message from "./Message";
 import {v4 as uuid} from 'uuid'
 import {useRedux} from "../hook/redux";
 
 const Messages = () => {
     const {channel, user, message} = useRedux();
+    const messagesRef = useRef();
+    const isCurrentChannelUser = channel.currentChannel?.users.some(u => u.id === user.currentUser.id)
+
+    useEffect(() => {
+        if (channel.currentChannel && isCurrentChannelUser) {
+            messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+        }
+    }, [channel.currentChannel]);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [message.messages]);
+
+    const scrollToBottom = () => {
+        if (messagesRef.current && messagesRef.current.scrollHeight) {
+            messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+        }
+    };
 
     if (!channel.currentChannel) {
         return <div className={mgs.messages}></div>
     }
 
     return (
-        <div className={mgs.messages}>
+        <div
+            className={mgs.messages}
+            ref={messagesRef}
+        >
             {
                 message.messages.map(mess =>
                         mess.channelId === channel.currentChannelId && (
