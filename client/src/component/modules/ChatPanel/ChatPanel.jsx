32a8d3aa5c52532aca19b00a2c04f8e2ec972d0e
Sako <give.me.heart8844@gmail.com>
2023-08-29 23:36:18 +0300

    Added connect user to channel.

--- client/src/component/modules/ChatPanel/ChatPanel.jsx
+++ client/src/component/modules/ChatPanel/ChatPanel.jsx
@@ -5,9 +5,12 @@ import {useRedux} from "../../../hook/redux";
 import InfoChannel from "../InfoChannel/InfoChannel";
 import SendMessages from "../SendMessages/SendMessages";
 import './ChatPanel.css';
+import Connect from "../../../UI/Connect/Connect";
+import ChannelAPI from "../../../http/channelAPI";
+import ChannelService from "../../../utils/reducer/service/channelService";
 
 const ChatPanel = ({websocket, isCurrentChannel}) => {
-    const {channel, user, message} = useRedux();
+    const {channel, user, message, dispatch} = useRedux();
     const messagesRef = useRef();
 
     const scrollToBottom = () => {
@@ -15,6 +18,22 @@ const ChatPanel = ({websocket, isCurrentChannel}) => {
             messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
         }
     };
+
+    const ConnectUserToChannel = async () => {
+        await ChannelAPI.addUserToChannelAPI(user.currentUser.id, channel.currentChannelId)
+            .then(channel =>
+                dispatch(
+                    ChannelService.addNewChannel(channel)
+                )
+            );
+
+        await ChannelAPI.fetchOneChannel(channel.currentChannelId)
+            .then(channel =>
+                dispatch(
+                    ChannelService.addCurrentChannel(channel)
+                )
+            );
+    }
 
     useEffect(() => {
         if (channel.currentChannel && isCurrentChannel) {
@@ -26,7 +45,7 @@ const ChatPanel = ({websocket, isCurrentChannel}) => {
         scrollToBottom();
     }, [message.messages]);
 
-    if (!isCurrentChannel) {
+    if (!channel.currentChannel) {
         return (
             <section className={'chat-panel'}></section>
         );
@@ -47,9 +66,20 @@ const ChatPanel = ({websocket, isCurrentChannel}) => {
                 >
                     <Messages />
                 </div>
-                <SendMessages
-                    websocket={websocket}
-                />
+
+                {
+                    isCurrentChannel ? (
+                        <SendMessages
+                            websocket={websocket}
+                        />
+                    ) : (
+                        <Connect
+                            onClick={ConnectUserToChannel}
+                        >
+                            Подключится
+                        </Connect>
+                    )
+                }
             </div>
         </section>
     );
